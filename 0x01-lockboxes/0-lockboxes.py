1830c51ae60ed2a79083776a401a6eba8ba4185a
madalafk <fmadala@hotmail.com>
2023-12-05 12:38:27 -0800

    0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,18 +6,19 @@ determines if all the boxes in n number of locked boxes  can be opened.
 
 def canUnlockAll(boxes):
     '''
-    Checks if all the boxes in a list of boxes containing the keys
-    (indices) to other boxes can be unlocked given that the first
+    Checking if all boxes in a list of boxes containing the keys
+    to other boxes can be unlocked given that the first
     box is unlocked.
+    Return True if all boxes can be opened, else return False
     '''
     n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
+    opened_boxes = set([0])
+    un_opened_boxes = set(boxes[0]).difference(set([0]))
+    while len(un_opened_boxes) > 0:
+        boxIdx = un_opened_boxes.pop()
         if not boxIdx or boxIdx >= n or boxIdx < 0:
             continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+        if boxIdx not in opened_boxes:
+            un_opened_boxes = un_opened_boxes.union(boxes[boxIdx])
+            opened_boxes.add(boxIdx)
+    return n == len(opened_boxes)
