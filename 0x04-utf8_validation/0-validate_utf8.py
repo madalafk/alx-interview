8698a714d999f9273407e03c5e9deb1342d8da07
madalafk <fmadala@hotmail.com>
2024-01-04 10:03:35 -0800

    0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
- a method that determines if a given data set represents
- a valid UTF-8 encoding.
+method that determines if a given data set represents
+a valid UTF-8 encoding.
 """
 
 def validUTF8(data):
